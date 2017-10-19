abc5a458815c85f0d5bdd15dda839b437d5dbd23
ricardo-lopez-people-media <32902389+ricardo-lopez-people-media@users.noreply.github.com>
2017-10-19 08:06:10 -0500

    agregar "remover" columnas carga de excel
    arreglo matchColumnDef

--- src/js/directives/cargaArchivoXls/carga-archivo-xls.directive.js
+++ src/js/directives/cargaArchivoXls/carga-archivo-xls.directive.js
@@ -1,4 +1,3 @@
-
 angular
     .module('RDash')
     .directive('importSheetJs', ['globalsController','$state','$rootScope','dataController',
@@ -12,48 +11,81 @@ angular
                     reader.onload = function (e) {
                         /* read workbook */
                         var bstr = e.target.result;
-                        var wb = XLSX.read(bstr, {type:'binary'});
+                        var wb = XLSX.read(bstr, {type: 'binary'});
 
                         /* grab first sheet */
-                        for(var i in wb.SheetNames){
+                        for (var i in wb.SheetNames) {
                             console.log(wb.SheetNames[i]);
                         }
+                        console.log(e);
 
                         var wsname = wb.SheetNames[0];
                         var ws = wb.Sheets[wsname];
 
                         /* grab first row and generate column headers */
-                        var aoa = XLSX.utils.sheet_to_json(ws, {header:1, raw:false, blankrows:false});
+                        var aoa = XLSX.utils.sheet_to_json(ws, {header: 1, raw: false, blankrows: false});
                         console.log(aoa);
 
-                        $.when(dc.insertResumenAseguradora(aoa)).done(function(response){
-                            console.log(response);
-                        }).fail(function (response) {
-                            console.log('Error en la llamada al servicio');
-                            console.log(response);
-                        }).then(function(response){
-                            $elm.val("");
-                        });
+                        var validCols = [];//aqui almacenamos si la columna n del archivo Excel esta presente en "opts.matchColumnDef"
+                        var cols = [];
+                        var data = [];
+                        var localMCD = $scope.opts.matchColumnDef;
+                        if (localMCD && localMCD.length ){ // si no lo mandas... nada será truncado
+                            var lengCD = localMCD.length;
+                            //Agregar e inicializar matchColumnDef.txFound
+                            for(var j=0; j < lengCD ; j++) {
+                                localMCD[j].txFound = false;
+                            }
+                            for(var i = 0; i < aoa[0].length; ++i){
+                                for(var j=0; j < lengCD ; j++) {
+                                    validCols[i] = (aoa[0][i].toUpperCase() === localMCD[j].txField.toUpperCase());
 
+                                    if (validCols[i]) {
+                                        localMCD[j].txFound = validCols[i];
+                                        break;
+                                    }
+                                }
 
+                            }
+                            for (var i = 0; i < aoa[0].length; ++i) {
+                                if(validCols[i]) {
+                                    cols.push({field: aoa[0][i]});
+                                }
+                            }
+                            // /* generate rest of the data */
+                            if (cols.length) { // si solo si, se tiene al menos una columna valida
+                                for (var r = 1; r < aoa.length; ++r) {
+                                    data[r - 1] = {};
+                                    for (i = 0; i < aoa[r].length; ++i) {
+                                        if(validCols[i] && aoa[r][i]) {
+                                            data[r - 1][aoa[0][i]] = aoa[r][i];
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        else {
+                            for (var i = 0; i < aoa[0].length; ++i) {
+                                cols[i] = {field: aoa[0][i]};
+                            }
+                            //
+                            // /* generate rest of the data */
 
-                        // var cols = [];
-                        // for(var i = 0; i < aoa[0].length; ++i) cols[i] = { field: aoa[0][i] };
-                        //
-                        // /* generate rest of the data */
-                        // var data = [];
-                        // for(var r = 1; r < aoa.length; ++r) {
-                        //     data[r-1] = {};
-                        //     for(i = 0; i < aoa[r].length; ++i) {
-                        //         if(aoa[r][i] == null) continue;
-                        //         data[r-1][aoa[0][i]] = aoa[r][i]
-                        //     }
-                        // }
-
+                            for (var r = 1; r < aoa.length; ++r) {
+                                data[r - 1] = {};
+                                for (i = 0; i < aoa[r].length; ++i) {
+                                    if (aoa[r][i] == null) continue;
+                                    data[r - 1][aoa[0][i]] = aoa[r][i]
+                                }
+                            }
+                        }
                         /* update scope */
                         $scope.$apply(function() {
-                            //$scope.opts.columnDefs = cols;
-                            //$scope.opts.data = data;
+                            $scope.opts.columnDefs = cols;
+                            $scope.opts.data = data;
+                            if (localMCD && localMCD.length ) {
+                               // $scope.opts.matchColumnDef = localMCD;
+                            }
                         });
                     };
 
