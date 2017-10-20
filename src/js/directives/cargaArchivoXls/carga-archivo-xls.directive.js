f8849d9e85d66458edf257b380e1efb4fe4d039f
ricardo-lopez-people-media <32902389+ricardo-lopez-people-media@users.noreply.github.com>
2017-10-19 19:43:38 -0500

    agregado de dos mensajes en caso de NO excel

--- src/js/directives/cargaArchivoXls/carga-archivo-xls.directive.js
+++ src/js/directives/cargaArchivoXls/carga-archivo-xls.directive.js
@@ -1,3 +1,4 @@
+
 angular
     .module('RDash')
     .directive('importSheetJs', ['globalsController','$state','$rootScope','dataController',
@@ -10,85 +11,97 @@ angular
 
                     reader.onload = function (e) {
                         /* read workbook */
-                        var bstr = e.target.result;
-                        var wb = XLSX.read(bstr, {type: 'binary'});
-
-                        /* grab first sheet */
-                        for (var i in wb.SheetNames) {
-                            console.log(wb.SheetNames[i]);
-                        }
-                        console.log(e);
-
-                        var wsname = wb.SheetNames[0];
-                        var ws = wb.Sheets[wsname];
-
-                        /* grab first row and generate column headers */
-                        var aoa = XLSX.utils.sheet_to_json(ws, {header: 1, raw: false, blankrows: false});
-                        console.log(aoa);
-
                         var validCols = [];//aqui almacenamos si la columna n del archivo Excel esta presente en "opts.matchColumnDef"
                         var cols = [];
                         var data = [];
                         var localMCD = $scope.opts.matchColumnDef;
-                        if (localMCD && localMCD.length ){ // si no lo mandas... nada será truncado
-                            var lengCD = localMCD.length;
-                            //Agregar e inicializar matchColumnDef.txFound
-                            for(var j=0; j < lengCD ; j++) {
-                                localMCD[j].txFound = false;
+                        var localMsgError=[];// = $scope.opts.MessageErrors;
+                        var bstr = e.target.result;
+                        var wb = undefined;
+                        try{
+                            wb = XLSX.read(bstr, {type: 'binary'});
+                            /* grab first sheet */
+                            for (var i in wb.SheetNames) {
+                                console.log(wb.SheetNames[i]);
                             }
-                            for(var i = 0; i < aoa[0].length; ++i){
+                            console.log("e wb")
+                            console.log(e);
+                            console.log(wb);
+                            var wsname = wb.SheetNames[0];
+                            var ws = wb.Sheets[wsname];
+
+                            /* grab first row and generate column headers */
+                            var aoa = XLSX.utils.sheet_to_json(ws, {header: 1, raw: false, blankrows: false});
+                            console.log(aoa);
+
+                            if (localMCD && localMCD.length ){ // si no lo mandas... nada será truncado
+                                var lengCD = localMCD.length;
+                                //Agregar e inicializar matchColumnDef.txFound
                                 for(var j=0; j < lengCD ; j++) {
-                                    validCols[i] = (aoa[0][i].toUpperCase() === localMCD[j].txField.toUpperCase());
-
-                                    if (validCols[i]) {
-                                        localMCD[j].txFound = validCols[i];
-                                        break;
-                                    }
+                                    localMCD[j].txFound = false;
                                 }
+                                //Buscsr l columnas
+                                for(var i = 0; i < aoa[0].length; ++i){
+                                    for(var j=0; j < lengCD ; j++) {
+                                        validCols[i] = (aoa[0][i].toUpperCase() === localMCD[j].txField.toUpperCase());
 
-                            }
-                            for (var i = 0; i < aoa[0].length; ++i) {
-                                if(validCols[i]) {
-                                    cols.push({field: aoa[0][i]});
-                                }
-                            }
-                            // /* generate rest of the data */
-                            if (cols.length) { // si solo si, se tiene al menos una columna valida
-                                for (var r = 1; r < aoa.length; ++r) {
-                                    data[r - 1] = {};
-                                    for (i = 0; i < aoa[r].length; ++i) {
-                                        if(validCols[i] && aoa[r][i]) {
-                                            data[r - 1][aoa[0][i]] = aoa[r][i];
+                                        if (validCols[i]) {
+                                            localMCD[j].txFound = validCols[i];
+                                            //copiar solo las columnas qtenn ambos : excel y matchColumnDef
+                                            cols.push({field: localMCD[j].txField, name: aoa[0][i], displayName: aoa[0][i]});
+                                            aoa[0][i] = localMCD[j].txField; //Temporalmente se asigna el CaMel del txField, para que sea identico al de la BD
+                                            break;
                                         }
                                     }
                                 }
-                            }
-                        }
-                        else {
-                            for (var i = 0; i < aoa[0].length; ++i) {
-                                cols[i] = {field: aoa[0][i]};
-                            }
-                            //
-                            // /* generate rest of the data */
-
-                            for (var r = 1; r < aoa.length; ++r) {
-                                data[r - 1] = {};
-                                for (i = 0; i < aoa[r].length; ++i) {
-                                    if (aoa[r][i] == null) continue;
-                                    data[r - 1][aoa[0][i]] = aoa[r][i]
+                                // /* generate rest of the data */
+                                if (cols.length) { // si solo si, se tiene al menos una columna valida
+                                    for (var r = 1; r < aoa.length; ++r) {
+                                        data[r - 1] = {};
+                                        for (i = 0; i < aoa[r].length; ++i) {
+                                            if(validCols[i] && aoa[r][i]) {
+                                                data[r - 1][aoa[0][i]] = aoa[r][i];
+                                            }
+                                        }
+                                    }
+                                }
+                                else{
+                                    localMsgError.push({msg:"la primer linea de la hoja de excel, debe contener el nombre de las columnas", tipo:"danger", dismiss:"alert"});
                                 }
                             }
-                        }
-                        /* update scope */
-                        $scope.$apply(function() {
-                            $scope.opts.columnDefs = cols;
-                            $scope.opts.data = data;
-                            if (localMCD && localMCD.length ) {
-                               // $scope.opts.matchColumnDef = localMCD;
-                            }
-                        });
-                    };
+                            else {
+                                for (var i = 0; i < aoa[0].length; ++i) {
+                                    cols[i] = {field: aoa[0][i]};
+                                }
+                                //
+                                // /* generate rest of the data */
 
+                                for (var r = 1; r < aoa.length; ++r) {
+                                    data[r - 1] = {};
+                                    for (i = 0; i < aoa[r].length; ++i) {
+                                        if (aoa[r][i] == null) continue;
+                                        data[r - 1][aoa[0][i]] = aoa[r][i]
+                                    }
+                                }
+                            }
+                            }
+                        catch(error){
+                            console.log(error);
+                            localMsgError.push({msg:error.toString(), tipo:"danger", dismiss:"alert"});
+                        }
+                        finally {
+                            /* update scope */
+                            $scope.$apply(function() {
+                                $scope.opts.columnDefs = cols;
+                                $scope.opts.data = data;
+                                $scope.opts.MessageErrors = localMsgError;
+                                if (localMCD && localMCD.length ) {
+                                    $scope.opts.matchColumnDef = localMCD;
+                                }
+                            });
+                        };
+
+                    };
                     reader.readAsBinaryString(changeEvent.target.files[0]);
                 });
             }
