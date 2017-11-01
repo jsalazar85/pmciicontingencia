14d5ebd0c1b8ee8d3fc9f07023b6c29500258eb0
rubenvertiz <32936179+rubenvertiz@users.noreply.github.com>
2017-10-31 19:19:41 -0600

    Cambios para exportación a excel y pptx

--- src/views/tableroReporteGlobal/tablero-reporte-global-uso-reserva.view.js
+++ src/views/tableroReporteGlobal/tablero-reporte-global-uso-reserva.view.js
@@ -24,28 +24,86 @@ angular
                 {"id": 5, "medida": "Donativo Fundación Hogares", "beneficiados": "-", "monto": "-"}
             ];
             $scope.rows=rows;
+            $scope.imagenes=[];
 
-            /* $scope.getChart1 = function() {
-
-                cds.addWorkTask('iniciativa.indicador', {
-                    url: gc.conf.xsServicesBaseUrl + '/dbTableroIniciativasGenerico.xsjs',
-                    query: {
-                        idIniciativa: $scope.idIniciativa
-                    },
-                    success: function(response) {
-                        console.log("success");
-                        console.log(response);
-                        //$scope.censo.data=response.data;
-
-                        $scope.setChart1Data(response);
-
-                    },
-                    error: function(response, error) {
-                        console.log("Error");
-                        console.log(error);
+            // OBTIENE IMAGEN POR NODO
+            function generaImagenAsync(nodo){
+                console.log("generaImagenAsync");
+                var deferred = $.Deferred();
+                console.log('nodo:'+nodo);
+                domtoimage.toBlob(nodo)
+                    .then(function(blob) {
+                        var reader = new window.FileReader();
+                        reader.readAsDataURL(blob);
+                        var df2=deferred;
+                        reader.onloadend = function() {
+                            base64data = reader.result;
+                            $scope.imagenes.push(base64data);
+                            console.log($scope.imagenes);
+                            df2.resolve(base64data);
+                        }
+                    });
+                return deferred.promise();
+            }
+            // FUNCION PARA GENERAR PPTX
+            $scope.generaPPTX = function(){
+                $scope.imagenes=[];
+                //obtenemos los nodos a generar imagenes
+                console.log("generaPPTX");
+                var nodos = document.getElementsByClassName("texportarUR-pptx");
+                var promesas = [];
+                console.log(nodos);
+                console.log(nodos.length);
+                //obtenemos las imagenes por cada nodo
+                for(var i=0; i<nodos.length;i++){
+                    promesas.push(generaImagenAsync(nodos[i]));
+                }
+                console.log($scope.imagenes);
+                $.when.apply($, promesas).then(function() {
+                    console.log("imagenes:"+$scope.imagenes.length);
+                    // generamos el pptx
+                    var pptx = new PptxGenJS();
+                    for(var i=0; i<$scope.imagenes.length;i++){
+                        var slide = pptx.addNewSlide();
+                        slide.addImage({
+                            data: $scope.imagenes[($scope.imagenes.length-1)-i],
+                            x: 1.0,
+                            y: 0.5,
+                            w: 7.0,
+                            h: 5.0
+                        });
                     }
+                    pptx.save('ReporteUsoReservas');
+                }, function(e) {
+                    console.log("error");
                 });
-            }; */
+            }
+
+            $scope.generaXLSX = function(){
+                var nodos = document.getElementsByClassName("texportarUR-pptx");
+                var nodo = nodos[0];
+                console.log(nodo)
+                var wb = XLSX.utils.table_to_book(nodo, {sheet:"Iniciativa"});
+                var wbout = XLSX.write(wb, {bookType:'xlsx', bookSST:true, type: 'binary'});
+                var fname = 'ReporteUsoReservas.xlsx';
+                try {
+                    saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), fname);
+                } catch(e) { if(typeof console != 'undefined') console.log(e, wbout); }
+                return wbout;
+            }
+
+            function s2ab(s) {
+                if(typeof ArrayBuffer !== 'undefined') {
+                    var buf = new ArrayBuffer(s.length);
+                    var view = new Uint8Array(buf);
+                    for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+                    return buf;
+                } else {
+                    var buf = new Array(s.length);
+                    for (var i=0; i!=s.length; ++i) buf[i] = s.charCodeAt(i) & 0xFF;
+                    return buf;
+                }
+            }
 
             $scope.getTableData = function() {
 
